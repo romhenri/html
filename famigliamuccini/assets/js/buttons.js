1e983dbfc9a70af33c01c9d3f49f49c958352814
Rômulo Henri <123867521+Henri2023@users.noreply.github.com>
2023-02-23 19:31:26 -0300

    Atualizando

--- famigliamuccini/assets/js/buttons.js
+++ famigliamuccini/assets/js/buttons.js
@@ -15,3 +15,31 @@ function pinterest() {
 
     window.open('https://br.pinterest.com/famigliamucciniristorante/');
 }
+
+
+function linkedin() {
+    console.log('Usuário direcionado ao Linkedin.');
+
+    window.open('https://www.linkedin.com/company/famigliamuccini/');
+}
+
+
+
+
+
+
+
+const TXT = document.getElementById('meet')
+
+
+function next() {
+    TXT.innerHTML= (`
+    
+    <h2>Teste</h2>
+         
+    <div id="buttonsLine">
+            <input type="button" value="Saber Mais" class="buttonB" id="nextButton">
+            <input type="button" value="Avançar" class="button" id="nextButton" onclick="next()">
+          </div>
+    `);
+}
