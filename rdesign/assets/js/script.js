482b81bce74480db50edd20bde875f5b6636658c
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-04-23 22:28:44 -0300

    Atualizando

---
+++ rdesign/assets/js/script.js
@@ -0,0 +1,7 @@
+var btnPricing = document.getElementById("pricing");
+
+btnPricing.addEventListener("click", () => {
+	alert(
+		"Calculadora de Orçamento Indisponível! Tente novamente a partir do dia 24/04 ás 20:00."
+	);
+});
